4a71bda8092b1d695a9b90e5b72d5b8e2cfc7d6d
Anand Chowdhary <anandchowdhary@gmail.com>
2016-10-20 23:00:06 +0530

    Start 2-day project

---
+++ admin/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+	<head>
+
+        <title>Admin Panel</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+		<link href="https://cdn.oswald.foundation/34c62-bootstrap.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+
+        <nav>
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a href="#" class="navbar-brand">Admin</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Posts</a></li>
+                    <li><a href="#">Create</a></li>
+                </ul>
+            </div>
+        </nav>
+
+    </body>
+
+</html>
